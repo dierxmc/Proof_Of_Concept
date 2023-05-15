bf76d213f42c618b7f5e755134efb13fee79ceb9
dierxmc <40238021+dierxmc@users.noreply.github.com>
2023-05-15 09:53:31 +0200

    Update ProofOfConcept.py

--- myapp/ProofOfConcept.py
+++ myapp/ProofOfConcept.py
@@ -1,7 +1,7 @@
 import pyfiglet
 title = pyfiglet.figlet_format(' Jenkins', font='slant')
 title2 = pyfiglet.figlet_format('Proof Of Concept ', font='slant')
-title3 = pyfiglet.figlet_format('Test ', font='slant')
+title3 = pyfiglet.figlet_format('Hallo ', font='slant')
 print(title)
 print(title2)
 print(title3)
